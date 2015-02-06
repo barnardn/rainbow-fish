5c0f278c953e66ed91f4e789fda151c6f4f79764
Norm Barnard <barnard@clamdango.com>
2015-02-05 21:24:23 -0500

    text edits

--- rainbow-fish/ProductTableViewCell.swift
+++ rainbow-fish/ProductTableViewCell.swift
@@ -23,13 +23,11 @@ class ProductTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-
         self.accessoryType = .DisclosureIndicator
         self.separatorInset = UIEdgeInsets(top: 0, left: 20.0, bottom: 0, right: 0)
         self.selectedBackgroundView = UIView(frame: self.bounds)
         self.selectedBackgroundView.backgroundColor = AppearanceManager.appearanceManager.selectedCellBackgroundColor
         self.contentView.backgroundColor = UIColor.whiteColor()
-        
         titleLabel.font = AppearanceManager.appearanceManager.standardFont
         titleLabel.textColor = AppearanceManager.appearanceManager.bodyTextColor
     }
