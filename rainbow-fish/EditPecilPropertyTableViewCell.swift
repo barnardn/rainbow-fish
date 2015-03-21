c4d9756462687e9db5a2a3b797dedd2d604671f4
Norm Barnard <barnard@clamdango.com>
2015-03-21 15:11:38 -0400

    compute new string correctly based on range of edits

--- rainbow-fish/EditPecilPropertyTableViewCell.swift
+++ rainbow-fish/EditPecilPropertyTableViewCell.swift
@@ -74,7 +74,9 @@ extension EditPecilPropertyTableViewCell: UITextFieldDelegate {
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
         if let keypath = self.keyPath {
             if let pencil = self.pencil {
-                var value = textfield.text + string
+                var end = advance(textField.text.startIndex, range.location)
+                let replaceRange: Range<String.Index> = Range<String.Index>(start: textField.text.startIndex, end: end)
+                var value = textfield.text.substringWithRange(replaceRange) + string
                 pencil.setValue(value, forKey: keypath)
             }
         }
